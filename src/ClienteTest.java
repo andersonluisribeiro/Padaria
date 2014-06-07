07bedd65eb013cab6e0b8deee51e045db1d38f1d
Anderson Luis Ribeiro <desenvolvimento3@unifeob.edu.br>
2014-06-07 16:46:45 -0300

    Criando teste do metodo que calcula o total das compras do cliente

---
+++ src/ClienteTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class ClienteTest {
+
+	@Test
+	public void deveCalcularOValorTotalDasCompras() {
+		
+	}
+
+}
--- test/AllTests.java
+++ test/AllTests.java
@@ -4,7 +4,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 
 @RunWith(Suite.class)
-@SuiteClasses({ ItemTest.class, VendaTest.class })
+@SuiteClasses({ ItemTest.class, VendaTest.class, ClienteTest.class })
 public class AllTests {
 
 }
